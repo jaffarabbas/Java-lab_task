53698d9b09c60429c24a7783975776c85d5fcf2e
jaffarabbas <gamaportal8@gmail.com>
2025-11-22 14:01:04 +0500

    Create quick.java

---
+++ quicksort/quick.java
@@ -0,0 +1,70 @@
+class Main{
+    public static void quickSort(int[] arr, int low , int high){
+        if(low < high){
+            int pivotIndex = partition(arr, low , high);
+            quickSort(arr, low , pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1 , high);
+        }
+    }
+    public static int partition(int[] arr, int low , int high){
+        int pivot = arr[low];
+        int i = low;
+        System.out.println("Pivot: " + pivot);
+        System.out.println("Low: " + low + ", High: " + high);
+        System.out.println("Array State: ");
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        for(int j = low + 1 ; j <= high ; j++){
+            System.out.println("Comparing: " + arr[j] + " with pivot: " + pivot);
+            if(arr[j] < pivot){
+                System.out.println("Swapping " + arr[j] + " and " + arr[i+1]);
+                i++;
+                System.out.println("Before Swap: ");
+                for(int num : arr){
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+                swap(arr, i , j);
+                System.out.println("After Swap: ");
+                for(int num : arr){
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+                System.out.println("Swapped " + arr[i] + " and " + arr[j]);
+            }
+        }
+        System.out.println("Placing pivot " + pivot + " at correct position by swapping " + arr[low] + " and " + arr[i]);
+        System.out.println("Before Swap: ");
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        swap(arr, low , i);
+        System.out.println("After Swap: ");
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        swap(arr, low , i);
+        System.out.println("After Swap: ");
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        return i;
+    }
+    public static void swap(int[] arr, int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void main(String[] args) {
+        int[] arr = {34, 7, 23, 32, 5, 62};
+        quickSort(arr, 0 , arr.length - 1);
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+}
